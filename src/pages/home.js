55917402bd9d8d7ddcfcc758f2e2e4b8c2b2d327
John <codebeast0420@gmail.com>
2023-02-26 02:15:44 +0800

    add touchableopacity

--- src/pages/home.js
+++ src/pages/home.js
@@ -21,9 +21,9 @@ const Home = () => {
 					<TouchableOpacity className='bg-[#242222] w-[80px] h-[30px] d-flex flex-row items-center justify-center rounded-bl-sm rounded-tl-sm'>
 						<Text className='text-white text-[14px]'>My</Text>
 					</TouchableOpacity>
-					<TouchableHighlight className='border-[#BBBBBB] border-[1px] bg-white w-[80px] h-[30px] d-flex flex-row items-center justify-center rounded-br-sm rounded-tr-sm'>
+					<TouchableOpacity className='border-[#BBBBBB] border-[1px] bg-white w-[80px] h-[30px] d-flex flex-row items-center justify-center rounded-br-sm rounded-tr-sm'>
 						<Text className='text-black text-[14px]'>Discover</Text>
-					</TouchableHighlight>
+					</TouchableOpacity>
 				</View>
 				<View className='w-full mt-[20px] border-[1px] border-[#BBBBBB] d-flex flex-column items-center py-[50px]'>
 					<Image source={require('../../assets/home/dog.png')} />
@@ -31,22 +31,20 @@ const Home = () => {
 				</View>
 				<View className='w-full d-flex flex-row items-center mt-[15px] justify-between'>
 					<View className='d-flex flex-row'>
-						<View className='d-flex flex-row items-center'>
-							<TouchableOpacity>
-								<Image source={require('../../assets/home/back.png')} />
-							</TouchableOpacity>
-							<Text className='px-[5px]'>Undo</Text>
-						</View>
-						<View className='d-flex flex-row items-center ml-[50px]'>
+						<TouchableOpacity className='d-flex flex-row items-center'>
+							<Image source={require('../../assets/home/back.png')} />
+							<Text className='px-[5px] text-[#101010]'>Undo</Text>
+						</TouchableOpacity>
+						<TouchableOpacity className='d-flex flex-row items-center ml-[50px]'>
 							<Image source={require('../../assets/home/clear.png')} />
-							<Text className='px-[5px]'>Clear</Text>
-						</View>
+							<Text className='px-[5px] text-[#101010]'>Clear</Text>
+						</TouchableOpacity>
 					</View>
-					<View>
-						<Text>Depict</Text>
+					<View className='d-flex flex-row items-center'>
+						<Text className='text-[16px] text-[#101010]'>Depict</Text>
 						<Switch
-							trackColor={{ false: '#767577', true: '#81b0ff' }}
-							thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+							trackColor={{ false: '#BBBBBB', true: '#242222' }}
+							thumbColor={'#FFFFFF'}
 							ios_backgroundColor="#3e3e3e"
 							onValueChange={toggleSwitch}
 							value={isEnabled}
